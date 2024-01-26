ac1d4ba3d2d67107547eda401073c623928674dc
OlexandrKyrkovsky <oleksandr.kyrkovsky@icloud.com>
2024-01-26 19:00:32 +0200

    doopratsiuvannia

--- hw3_2.py
+++ hw3_2.py
@@ -1,13 +1,16 @@
 import random
 def get_numbers_ticket(min, max, quantity):
     list=[]
-    while len(list)<max:
-        b=random.randint(min, max)
-        if b not in list:
-            list.append(b)
-    c=random.sample(list, quantity)
-    d=sorted(c)
-    return d
+    if min<max:
+        while len(list)<max:
+            b=random.randint(min, max)
+            if b not in list:
+                list.append(b)
+        c=random.sample(list, quantity)
+        d=sorted(c)
+        return d
+    else:
+        print('не вірно вказані дані: min має бути менше max')
 
-lottery_numbers = get_numbers_ticket(1,100,6)
+lottery_numbers = get_numbers_ticket(100,50,6)
 print("Ваші лотерейні числа:", lottery_numbers)
